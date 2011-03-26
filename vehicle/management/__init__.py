0b83a6285628e5e2f3f6d8897809b105fb073a25
Daniel Naab <danielnaab@gmail.com>
2011-03-26 12:37:19 -0500

    Catch database exceptions thrown with django 1.3 with already-existing indices

--- vehicle/management/__init__.py
+++ vehicle/management/__init__.py
@@ -5,17 +5,25 @@ from vehicle import models
 def create_indices():
     print '*** Creating indices...'
     from django.db import connection
+    from django.db.utils import DatabaseError
     cursor = connection.cursor()
 
     # make/model is the primary sort order, so create an index on both
-    cursor.execute('CREATE INDEX index_make_model ON vehicle_epavehicle (model, manufacturer)')
+    try:
+        cursor.execute('CREATE INDEX index_make_model ON vehicle_epavehicle (model, manufacturer)')
+        print 'Index `index_make_model` created'
+    except DatabaseError, e:
+        print 'Index index_make_model already exists'
     
     # create index on individual columns.  these are the fields exposed in the vehicle search interface.
     index_columns = ['year', 'manufacturer', 'model', 'transmission', 'transmission_speed', 'vehicle_class']
     for column in index_columns:
-        print '***** Index: ' + column
         cmd = "CREATE INDEX index_%s ON vehicle_epavehicle (%s)" % (column, column)
-        cursor.execute(cmd)
+        try:
+            cursor.execute(cmd)
+            print 'Index `%s` created' % column
+        except DatabaseError, e:
+            print 'Index index_%s already exists' % column
 
 def init_data(sender, **kwargs):
     create_indices()
